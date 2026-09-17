9c4e6bb47ad2fb489b53ec1e589f1a62ad6b1d26
Chromiell <chromiell@gmail.com>
2026-09-17 16:48:49 +0200

    Neovim: updated php-cs-fixer config to give precedence to the config file in the project root if present, otherwise the generic one in the .config directory

--- neovim/.config/nvim/lua/plugins/php-cs-fixer.lua
+++ neovim/.config/nvim/lua/plugins/php-cs-fixer.lua
@@ -9,12 +9,22 @@ return {
             php_cs_fixer = {
                 command = "php-cs-fixer",
                 timeout_ms = 30000,
-                args = {
-                    "fix",
-                    "--config=" .. vim.fn.expand("~/.config/php-cs-fixer/.php-cs-fixer.php"),
-                    "--using-cache=no",
-                    "$FILENAME",
-                },
+                args = function(ctx)
+                    -- Search upward from the active file's directory for a project config
+                    local local_config = vim.fs.find({ ".php-cs-fixer.php", ".php-cs-fixer.dist.php" }, {
+                        upward = true,
+                        path = ctx.dirname,
+                    })[1]
+
+                    local config_path = local_config or vim.fn.expand("~/.config/php-cs-fixer/.php-cs-fixer.php")
+
+                    return {
+                        "fix",
+                        "--config=" .. config_path,
+                        "--using-cache=no",
+                        "$FILENAME",
+                    }
+                end,
                 stdin = false,
             },
         },
